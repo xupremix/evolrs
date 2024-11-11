7ba266424cc0abe74c7139ef96c67dced67024db
xupremix <xupremix.me@gmail.com>
2024-11-11 05:00:25 +0100

    item half precision float

--- examples/tensor/src/main.rs
+++ examples/tensor/src/main.rs
@@ -1,11 +1,13 @@
 use evolrs::{
     device::Cpu,
-    kind::{c16, c32, c64},
-    shapes::shape::{Rank1, Rank2},
-    tensor::{gen::logspace::LogspaceScalar, Tensor},
+    kind::{c16, c32, c64, f16},
+    shapes::shape::{Rank1, Rank2, Rank3},
+    tensor::{gen::logspace::LogspaceScalar, wrap::item::Item, Tensor},
 };
 
 fn main() {
-    let t: Tensor<Rank1<2>, Cpu, u8> = Tensor::randint(0, 10);
+    let t: Tensor<Rank3<1, 1, 1>, Cpu, f16> = Tensor::rand();
+    let v = t.item();
+    println!("{:?}", v);
     t.print();
 }
