14a4be06000110ecb06918b586739b5d740b54e7
xupremix <xupremix.me@gmail.com>
2024-11-23 16:17:44 +0100

    more scalar fixes

--- crates/evolrs_core/src/ops/scalar_ops.rs
+++ crates/evolrs_core/src/ops/scalar_ops.rs
@@ -1,13 +1,14 @@
 use crate::kind::{
+    restriction::composite::IntOrBool,
     scalar::IntoScalar,
     type_coercion::{Coerce, DivCoerce, Same},
 };
 use crate::{device::Device, shapes::shape::Shape, tensor::Tensor};
 
 macro_rules! op {
-    ($($coerce:ident $trait:ident $assign_trait:ident $method:ident $tch_method:ident $assign_method:ident $tch_assign_method:ident),* $(,)?) => {
+    ($($coerce:ident $trait:ident $assign_trait:ident $method:ident $tch_method:ident $assign_method:ident $tch_assign_method:ident $($restr:ident)?),* $(,)?) => {
         $(
-            impl<T: IntoScalar, S: Shape, D: Device, K: $coerce<T>> std::ops::$trait<T> for Tensor<S, D, K> {
+            impl<T: IntoScalar $(+ $restr)?, S: Shape, D: Device, K: $coerce<T> $(+ $restr)?> std::ops::$trait<T> for Tensor<S, D, K> {
                 type Output = Tensor<S, D, K::To>;
 
                 fn $method(self, rhs: T) -> Self::Output {
@@ -17,7 +18,7 @@ macro_rules! op {
                     }
                 }
             }
-            impl<T: IntoScalar, S: Shape, D: Device, K: $coerce<T>> std::ops::$trait<T> for &Tensor<S, D, K> {
+            impl<T: IntoScalar $(+ $restr)?, S: Shape, D: Device, K: $coerce<T> $(+ $restr)?> std::ops::$trait<T> for &Tensor<S, D, K> {
                 type Output = Tensor<S, D, K::To>;
 
                 fn $method(self, rhs: T) -> Self::Output {
@@ -27,7 +28,7 @@ macro_rules! op {
                     }
                 }
             }
-            impl<T: IntoScalar, S: Shape, D: Device, K: $coerce<T>> std::ops::$assign_trait<T>
+            impl<T: IntoScalar $(+ $restr)?, S: Shape, D: Device, K: $coerce<T> $(+ $restr)?> std::ops::$assign_trait<T>
                 for Tensor<S, D, K>
             where
                 K: Same<K::To>,
@@ -46,17 +47,24 @@ macro_rules! op {
                 Coerce Sub sub g_sub_scalar,
                 Coerce Mul mul g_mul_scalar,
                 DivCoerce Div div g_div_scalar,
-                Coerce BitAnd bitand bitwise_and,
-                Coerce BitOr bitor bitwise_or,
-                Coerce BitXor bitxor bitwise_xor,
-                Coerce Shl shl bitwise_left_shift_tensor_scalar,
-                Coerce Shr shr bitwise_right_shift_tensor_scalar,
             }
         )*
     };
-    (@impl $t:ty => $($coerce:ident $trait:ident $method:ident $tch_fn:ident),* $(,)? ) => {
+    (@restr $($t:ty)*) => {
         $(
-            impl<S: Shape, D: Device, K: $coerce<$t>> std::ops::$trait<Tensor<S, D, K>> for $t {
+            op! {
+                @impl $t =>
+                Coerce BitAnd bitand bitwise_and IntOrBool,
+                Coerce BitOr bitor bitwise_or IntOrBool,
+                Coerce BitXor bitxor bitwise_xor IntOrBool,
+                Coerce Shl shl bitwise_left_shift_tensor_scalar IntOrBool,
+                Coerce Shr shr bitwise_right_shift_tensor_scalar IntOrBool,
+            }
+        )*
+    };
+    (@impl $t:ty => $($coerce:ident $trait:ident $method:ident $tch_fn:ident $($restr:ident)?),* $(,)? ) => {
+        $(
+            impl<S: Shape, D: Device, K: $coerce<$t> $(+ $restr)?> std::ops::$trait<Tensor<S, D, K>> for $t {
                 type Output = Tensor<S, D, K::To>;
 
                 fn $method(self, rhs: Tensor<S, D, K>) -> Self::Output {
@@ -66,7 +74,7 @@ macro_rules! op {
                     }
                 }
             }
-            impl<S: Shape, D: Device, K: $coerce<$t>> std::ops::$trait<&Tensor<S, D, K>> for $t {
+            impl<S: Shape, D: Device, K: $coerce<$t> $(+ $restr)?> std::ops::$trait<&Tensor<S, D, K>> for $t {
                 type Output = Tensor<S, D, K::To>;
 
                 fn $method(self, rhs: &Tensor<S, D, K>) -> Self::Output {
@@ -85,14 +93,17 @@ op! {
     Coerce Sub SubAssign sub g_sub_scalar sub_assign g_sub_scalar_,
     Coerce Mul MulAssign mul g_mul_scalar mul_assign g_mul_scalar_,
     DivCoerce Div DivAssign div g_div_scalar div_assign g_div_scalar_,
-    Coerce BitAnd BitAndAssign bitand bitwise_and bitand_assign bitwise_and_,
-    Coerce BitOr BitOrAssign bitor bitwise_or bitor_assign bitwise_or_,
-    Coerce BitXor BitXorAssign bitxor bitwise_xor bitxor_assign bitwise_xor_,
-    Coerce Shl ShlAssign shl bitwise_left_shift_tensor_scalar shl_assign bitwise_left_shift_tensor_scalar_,
-    Coerce Shr ShrAssign shr bitwise_right_shift_tensor_scalar shr_assign bitwise_right_shift_tensor_scalar_,
+    Coerce BitAnd BitAndAssign bitand bitwise_and bitand_assign bitwise_and_ IntOrBool,
+    Coerce BitOr BitOrAssign bitor bitwise_or bitor_assign bitwise_or_ IntOrBool,
+    Coerce BitXor BitXorAssign bitxor bitwise_xor bitxor_assign bitwise_xor_ IntOrBool,
+    Coerce Shl ShlAssign shl bitwise_left_shift_tensor_scalar shl_assign bitwise_left_shift_tensor_scalar_ IntOrBool,
+    Coerce Shr ShrAssign shr bitwise_right_shift_tensor_scalar shr_assign bitwise_right_shift_tensor_scalar_ IntOrBool,
 }
 op! {
     @inv u8 i8 i16 i32 i64 f32 f64
+}
+op! {
+    @restr u8 i8 i16 i32 i64 bool
 }
 #[cfg(feature = "half")]
 use crate::kind::f16;
--- examples/tensor/src/main.rs
+++ examples/tensor/src/main.rs
@@ -1,8 +1,7 @@
-use evolrs::{device::Cpu, shapes::shape::Rank2, tensor::Tensor};
+use evolrs::{device::Cpu, shapes::shape::Rank2, tch, tensor::Tensor};
 
 fn main() {
     let t: Tensor<Rank2<2, 3>, Cpu, i32> = Tensor::ones();
-    let t2: Tensor<_, _, i16> = Tensor::ones();
-    let ris = (t + t2 * 4).bitwise_left_shift(true);
+    let ris = t.add_s(2.0);
     ris.print();
 }
