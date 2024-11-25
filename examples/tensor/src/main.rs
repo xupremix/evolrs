387b1e77677f55afddc586d3110b9b2000a05246
xupremix <xupremix.me@gmail.com>
2024-11-25 01:17:44 +0100

    example of a backward pass with tensor checking

--- examples/tensor/src/main.rs
+++ examples/tensor/src/main.rs
@@ -1,5 +1,7 @@
 use evolrs::{
-    nn::{modules::linear::Linear, Module},
+    device::Cpu,
+    nn::{modules::linear::Linear, optim::Sgd, Module},
+    shapes::shape::Rank3,
     tch,
     tensor::Tensor3,
 };
@@ -9,7 +11,9 @@ fn main() {
     let root = &vs.root();
     let lin: Linear<3, 4> = Linear::new(root / "lin", Default::default());
     let xs: Tensor3<1, 2, 3> = Tensor3::rand();
+    let mut sgd: Sgd<Cpu> = Sgd::new(&vs, 0.01).unwrap();
 
-    let f = lin.forward(&xs);
-    f.print();
+    let l = lin.forward(&xs);
+
+    sgd.backward::<Rank3<1, 2, 3>, Linear<3, 4>>(&l);
 }
